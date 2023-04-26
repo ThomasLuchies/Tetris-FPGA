80d5b1f8ff9fff4eabe65b6487b399a55c158827
Thomas Luchies <55792684+ThomasLuchies@users.noreply.github.com>
2023-04-26 14:10:01 +0200

    werkend signaal

--- image_generator.vhd
+++ image_generator.vhd
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 --
---   FileName:         hw_image_generator.vhd
+--   FileName:         image_generator.vhd
 --   Dependencies:     none
 --   Design Software:  Quartus II 64-bit Version 12.1 Build 177 SJ Full Version
 --
@@ -24,38 +24,42 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 ENTITY image_generator IS
-  GENERIC(
-    pixels_y :  INTEGER := 480;   --row that first color will persist until
-    pixels_x :  INTEGER := 640);  --column that first color will persist until
-  PORT(
-    disp_ena :  IN   STD_LOGIC;  --display enable ('1' = display time, '0' = blanking time)
-    row      :  IN   INTEGER;    --row pixel coordinate
-    column   :  IN   INTEGER;    --column pixel coordinate
-    red      :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
-    green    :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
-    blue     :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')); --blue magnitude output to DAC
-end entity image_generator;
+	GENERIC(
+		pixels_y :	INTEGER := 480;    --row that first color will persist until
+		pixels_x	:	INTEGER := 640;
+		lastPixel : INTEGER := 0;   --column that first color will persist until
+		currentPixel : INTEGER := 0);
+	PORT(
+		disp_ena		:	IN		STD_LOGIC;	--display enable ('1' = display time, '0' = blanking time)
+		row			:	IN		INTEGER;		--row pixel coordinate
+		column		:	IN		INTEGER;		--column pixel coordinate
+		red			:	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
+		green			:	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
+		blue			:	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')); --blue magnitude output to DAC
+END image_generator;
 
 ARCHITECTURE behavior OF image_generator IS
 BEGIN
-  PROCESS(disp_ena, row, column)
-  BEGIN
-
-    IF(disp_ena = '1') THEN        --display time
-      IF(row < pixels_y AND column < pixels_x) THEN
-        red <= (OTHERS => '0');
-        green  <= (OTHERS => '0');
-        blue <= (OTHERS => '1');
-      ELSE
-        red <= (OTHERS => '1');
-        green  <= (OTHERS => '1');
-        blue <= (OTHERS => '0');
-      END IF;
-    ELSE                           --blanking time
-      red <= (OTHERS => '0');
-      green <= (OTHERS => '0');
-      blue <= (OTHERS => '0');
-    END IF;
-  
-  END PROCESS;
+	
+	PROCESS(disp_ena, row, column)
+	BEGIN
+		IF(disp_ena = '1') THEN		--display time
+			IF(row < pixels_y AND column < pixels_x) THEN
+				IF(column MOD 2) = 0 THEN	
+					red <= x"FF";
+					green	<= x"FF";
+					blue <= x"FF";
+				ELSE
+					red <= x"00";
+					green <= x"00";
+					blue <= x"00";
+				END IF;
+			END IF;
+		ELSE								--blanking time
+			red <= (OTHERS => '0');
+			green <= (OTHERS => '0');
+			blue <= (OTHERS => '0');
+		END IF;
+	
+	END PROCESS;
 END behavior;
--- tetris.vhd
+++ tetris.vhd
@@ -7,15 +7,15 @@ entity tetris is
 		reset_n		: std_logic := '0'
 	);
 	port(
-		CLOCK_50		:  in    std_logic;
+		clk		:  in    std_logic;
 		h_sync		:	OUT	STD_LOGIC;	--horiztonal sync pulse
 		v_sync		:	OUT	STD_LOGIC;	--vertical sync pulse
 		n_blank		:	OUT	STD_LOGIC;	--direct blacking output to DAC
 		n_sync		:	OUT	STD_LOGIC; --sync-on-green output to DAC
-		VGA_R       :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
-		VGA_G    	:  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
-		VGA_B    	:  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0'); --blue magnitude output to DAC
-		VGA_CLK		:  out	std_logic
+		red       :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0);  --red magnitude output to DAC
+		green    	:  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0);  --green magnitude output to DAC
+		blue    	:  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0); --blue magnitude output to DAC
+		VGA_clk		:  out	std_logic
 	);
 end entity;
 
@@ -23,24 +23,24 @@ architecture tetris_arch of tetris is
 	component pixel_clock is
 		port
 		(
-			areset		: IN STD_LOGIC  := '0';
-			inclk0		: IN STD_LOGIC  := '0';
+			areset		: IN STD_LOGIC;
+			inclk0		: IN STD_LOGIC;
 			c0				: OUT STD_LOGIC 
 		);
 	end component pixel_clock;
 
 	component vga_controller is
 		generic(
-			h_pulse 	:	INTEGER := 72;    	--horiztonal sync pulse width in pixels
-			h_bp	 	:	INTEGER := 128;		--horiztonal back porch width in pixels
-			h_pixels	:	INTEGER := 800;		--horiztonal display width in pixels
-			h_fp	 	:	INTEGER := 24;		   --horiztonal front porch width in pixels
-			h_pol		:	STD_LOGIC := '1';		--horizontal sync pulse polarity (1 = positive, 0 = negative)
-			v_pulse 	:	INTEGER := 2;			--vertical sync pulse width in rows
-			v_bp	 	:	INTEGER := 22;			--vertical back porch width in rows
-			v_pixels	:	INTEGER := 600;		--vertical display width in rows
-			v_fp	 	:	INTEGER := 1;			--vertical front porch width in rows
-			v_pol		:	STD_LOGIC := '1');	--vertical sync pulse polarity (1 = positive, 0 = negative)
+			h_pulse 	:	INTEGER;    	--horiztonal sync pulse width in pixels
+			h_bp	 	:	INTEGER;		--horiztonal back porch width in pixels
+			h_pixels	:	INTEGER;		--horiztonal display width in pixels
+			h_fp	 	:	INTEGER;		   --horiztonal front porch width in pixels
+			h_pol		:	STD_LOGIC;		--horizontal sync pulse polarity (1 = positive, 0 = negative)
+			v_pulse 	:	INTEGER;			--vertical sync pulse width in rows
+			v_bp	 	:	INTEGER;			--vertical back porch width in rows
+			v_pixels	:	INTEGER;		--vertical display width in rows
+			v_fp	 	:	INTEGER;			--vertical front porch width in rows
+			v_pol		:	STD_LOGIC);	--vertical sync pulse polarity (1 = positive, 0 = negative)
 		port( 
 			pixel_clk   : in std_logic;
 			reset_n		:	IN		STD_LOGIC;	--active low asycnchronous reset
@@ -56,15 +56,15 @@ architecture tetris_arch of tetris is
 	
 	component image_generator is
 	  generic(
-		 pixels_y :  INTEGER := 478;   --row that first color will persist until
-		 pixels_x :  INTEGER := 600);  --column that first color will persist until
+		 pixels_y :  INTEGER;   --row that first color will persist until
+		 pixels_x :  INTEGER);  --column that first color will persist until
 	  port(
 		 disp_ena :  IN   STD_LOGIC;  --display enable ('1' = display time, '0' = blanking time)
 		 row      :  IN   INTEGER;    --row pixel coordinate
 		 column   :  IN   INTEGER;    --column pixel coordinate
-		 red      :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
-		 green    :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
-		 blue     :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')); --blue magnitude output to DAC
+		 red      :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0);  --red magnitude output to DAC
+		 green    :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0);  --green magnitude output to DAC
+		 blue     :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)); --blue magnitude output to DAC
 	end component;
 	
 	signal row: integer;
@@ -72,31 +72,51 @@ architecture tetris_arch of tetris is
 	signal pixel_clk: std_logic;
 	signal disp_ena: std_logic;
 begin 
-	VGA_CLK <= pixel_clk;
-	pc0: pixel_clock port map(
-													areset => '0',
-													inclk0 => CLOCK_50,
-													c0 => pixel_clk
-												);
-	vga_c0: vga_controller port map(
-														pixel_clk => pixel_clk,
-														reset_n => '1',
-														h_sync => h_sync,
-														v_sync => v_sync,
-														disp_ena => disp_ena,
-														column => column,
-														row => row,
-														n_blank => n_blank,
-														n_sync => n_sync
-														);
+	VGA_clk <= pixel_clk;
+	
+	pc0: pixel_clock 
+	port map(
+		areset => '0',
+		inclk0 => clk,
+		c0 => pixel_clk
+	);
+	
+	vga_c0: vga_controller 
+	GENERIC MAP(h_bp => 48,
+			h_fp => 16,
+			h_pixels => 640,
+			h_pol => '0',
+			h_pulse => 96,
+			v_bp => 33,
+			v_fp => 10,
+			v_pixels => 480,
+			v_pol => '0',
+			v_pulse => 2
+	)
+	port map(
+		pixel_clk => pixel_clk,
+		reset_n => '1',
+		h_sync => h_sync,
+		v_sync => v_sync,
+		disp_ena => disp_ena,
+		column => row,
+		row => column,
+		n_blank => n_blank,
+		n_sync => n_sync
+	);
 
-	ig0: image_generator port map( 
-															disp_ena => disp_ena,
-															row => row,
-															column => column,
-															red => VGA_R,
-															green => VGA_G,
-															blue => VGA_B
-														);
+	ig0: image_generator 
+	GENERIC MAP(
+		pixels_x => 478,
+		pixels_y => 600
+	)
+	port map( 
+		disp_ena => disp_ena,
+		row => row,
+		column => column,
+		red => red,
+		green => green,
+		blue => blue
+	);
 															
 end architecture tetris_arch;
